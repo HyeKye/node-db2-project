9f7af49a8fbad6e320ce27b09262d1ae714cf4d9
Jordan <jordan.k.wilia@gmail.com>
2022-09-19 15:55:03 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '3HGCM56364G702777',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '3G1JC1245WS848211',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+        vin: 'JM1BG2246R0816241',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)  
+}
